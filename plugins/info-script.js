31561220084e9ce9b3876b0ad119b91d6c3b76fd
ArsyadDev1 <114202570+ArsyadDev1@users.noreply.github.com>
2022-10-07 06:42:24 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,23 +2,16 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*Sc Ini Tidak Di public Ataupun di Jual, Ini Cuman Kalian Yg memakai, Kalau mau Yah recode sendiri jir.*
 
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6282127487538`
+wa.me/6283193831650 (Owner)`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm,'makasih','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/_ctzhid",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'AR-MODS-DEV-V3 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
