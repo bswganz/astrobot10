58357132748b529e3bb8af49857c1390379c08bd
ArsyadDev1 <114202570+ArsyadDev1@users.noreply.github.com>
2022-10-06 15:09:13 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -6,25 +6,26 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let str = `${global.wm}
 
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
+│ • *axis:* [083193831650]
+│ • *axis2:* [083823763755]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
-│ • *Dana:* [${global.pdana}]
-│ • *Gopay:* [${global.pgopay}]
-│ • *Ovo:* [${global.povo}]
-│ • *Link Aja:* [${global.plinkaja}]
+│ • *Dana:* [6283823763755]
+│ • *Gopay:* [Pake Qris Aja]
+│ • *Ovo:* [Pake Qris Aja]
+│ • *Link Aja:* [Pake Qris Aja]
 ❏────`
-let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
+let wibu = `https://telegra.ph/file/1fc8425ad032e21b145bd.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/_ctzhid",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'AR-MODS-DEV-V3 MultiDevice',
     body: wm,
     thumbnail: thumb,
-    sourceUrl: sig
+    sourceUrl: sgc
   }
   } }) 
           }
